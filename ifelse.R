c1c0b2c9bfae0e1881fa646a37d49dd68473e2f7
cem <ca051@hdm-stuttgart.de>
2024-04-25 14:36:18 +0200

    TASK FUNCTION

--- ifelse.R
+++ ifelse.R
@@ -9,10 +9,10 @@ if(switch){
 }
 
 #LOOP TASK
-results <- vector("numeric")
+results <- c()
 for (i in 1:100){
   
-  if(i == 1){
+  if(i == 0){
     current_result <- i
   }
   else{
@@ -21,3 +21,31 @@ for (i in 1:100){
   results <- c(results, current_result)
 }
 print(results)
+
+#FUNCTION TO CONVERT CELCIUS TO FAHRENHEIT
+celsiusToFahrenheit <- function(celsius) {
+  fahrenheit <- (celsius * 1.8) + 32
+  return(fahrenheit)
+}
+fahrenheitToCelsius <- function(fahrenheit) {
+  celsius <- (fahrenheit -32) / 1.8
+  return(celsius)
+}
+celsius_value <- 100
+fahrenheit_value <- 212
+
+converted_fahrenheit_value <- celsiusToFahrenheit(celsius_value)
+converted_celsius_value <- fahrenheitToCelsius(fahrenheit_value)
+
+print(paste(converted_celsius_value, "degrees Celsius is", converted_fahrenheit_value, "degrees Fahrenheit"))
+
+#FUNCTION TASK 2
+data <- read.csv("pressure.tsv", sep="\t")
+
+data$temperatureCelsius <- sapply(data$temperature, fahrenheitToCelsius)
+
+print(data)
+
+
+
+
