621ffb02c3b60f633983eecca858e0c8cb30b2c6
cem <ca051@hdm-stuttgart.de>
2024-04-25 12:25:36 +0200

    loop task

--- ifelse.R
+++ ifelse.R
@@ -1,9 +1,23 @@
 setwd("~/sibe/GIT")
 switch <- FALSE
 
-
+#IF ELSE TASK
 if(switch){
   print("switch is on")
 }else{
   print("switch is broken")
 }
+
+#LOOP TASK
+results <- vector("numeric")
+for (i in 1:100){
+  
+  if(i == 1){
+    current_result <- i
+  }
+  else{
+    current_result <- i + results[i - 1]
+  }
+  results <- c(results, current_result)
+}
+print(results)
